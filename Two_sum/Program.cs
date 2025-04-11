3df043a06480e016db979896ceddf175f9811c35
Anwarali22 <ianwarali22@gmail.com>
2025-04-11 10:22:27 +0530

    Added Two_sum code[11-04-2025]

--- Two_sum/Program.cs
+++ Two_sum/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hijjjj, World!");
+﻿using System;
+
+namespace Two_sum
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+
+        #region Two_Sum
+        public int[] TwoSum(int[] nums, int target)
+        {
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+
+                }
+            }
+            return null;
+        }
+        #endregion
+    }
+}
